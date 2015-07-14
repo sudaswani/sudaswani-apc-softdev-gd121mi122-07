0b5e3cb2a903dedd3651d46f3df7822bedc38bee
sudaswani <syam090311@gmail.com>
2015-07-14 13:10:06 +0800

    created City.php model

---
+++ Practice/sudaswani/basic/models/City.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "city".
+ *
+ * @property integer $id
+ * @property string $city_code
+ * @property string $city_description
+ * @property string $citycol
+ * @property integer $province_id
+ */
+class City extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'city';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['city_code', 'city_description', 'citycol', 'province_id'], 'required'],
+            [['province_id'], 'integer'],
+            [['city_code', 'city_description'], 'string', 'max' => 32],
+            [['citycol'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'city_code' => 'City Code',
+            'city_description' => 'City Description',
+            'citycol' => 'Citycol',
+            'province_id' => 'Province ID',
+        ];
+    }
+}
