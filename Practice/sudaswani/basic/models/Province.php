194b6e86664fbaf235da4a62462197315577fce0
sudaswani <syam090311@gmail.com>
2015-07-14 13:11:35 +0800

    created Province.php model

---
+++ Practice/sudaswani/basic/models/Province.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "province".
+ *
+ * @property integer $id
+ * @property string $province_code
+ * @property string $province_description
+ * @property string $provincecol
+ * @property integer $region_id
+ */
+class Province extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'province';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['province_code', 'province_description', 'provincecol', 'region_id'], 'required'],
+            [['region_id'], 'integer'],
+            [['province_code', 'province_description'], 'string', 'max' => 32],
+            [['provincecol'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'province_code' => 'Province Code',
+            'province_description' => 'Province Description',
+            'provincecol' => 'Provincecol',
+            'region_id' => 'Region ID',
+        ];
+    }
+}
