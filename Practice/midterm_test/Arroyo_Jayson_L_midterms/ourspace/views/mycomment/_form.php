4cae984880eafec794df4ea390f2a81cb326e6cf
jlarroyo <jayson.arroyo36@gmail.com>
2015-07-21 13:21:05 +0800

    modify dropDownList

--- Practice/midterm_test/Arroyo_Jayson_L_midterms/ourspace/views/mycomment/_form.php
+++ Practice/midterm_test/Arroyo_Jayson_L_midterms/ourspace/views/mycomment/_form.php
@@ -2,7 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use yii\helpers\ArrayHelper;
+use app\models\Myaddress;
 /* @var $this yii\web\View */
 /* @var $model app\models\Mycomment */
 /* @var $form yii\widgets\ActiveForm */
@@ -12,7 +13,9 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'myaddress_id')->textInput() ?>
+      <?= $form->field($model, 'myaddress_id')->dropDownList(
+		ArrayHelper::map(Myaddress::find()->all(),'id','lastname')
+	) ?>
 
     <?= $form->field($model, 'atuhor')->textInput(['maxlength' => 255]) ?>
 
